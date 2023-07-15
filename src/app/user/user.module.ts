d468ba82909546551c08bd63d76e018ddcbae687
Ajay <ajayzyan100@gmail.com>
2023-07-15 08:03:50 +0530

    tailwind,shared module and some styles added

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
+
+
+@NgModule({
+  declarations: [
+    AuthModalComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AuthModalComponent
+  ]
+})
+export class UserModule { }
