d60671b2d3e281321f17a2eeb9473e32894bfcf8
Ajay <ajayzyan100@gmail.com>
2023-07-16 14:52:16 +0530

    input cmpnt added,ngx-mask,form completed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
 
 
 @NgModule({
@@ -10,14 +14,23 @@ import { TabComponent } from './tab/tab.component';
     ModalComponent,
     TabsContainerComponent,
     TabComponent,
+    InputComponent,
+    AlertComponent,
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe
   ],
   exports: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
-  ]
-})
+    TabComponent,
+    InputComponent,
+    AlertComponent
+  ],
+  providers: [
+    provideNgxMask()
+  ],})
 export class SharedModule { }
